4ee8739a80dd8b03120bbb69bd9bf6e3c2617cc9
Patron <Patron@Patrons-MacBook-Air.local>
2025-04-02 14:42:16 -0500

    DV

---
+++ CONSTANT.py
@@ -0,0 +1,11 @@
+FEATURES = ['Fp1', 'F3', 'C3', 'P3', 'F7', 'T3', 'T5', 'O1', 'Fz', 'Cz', 'Pz', 'Fp2', 'F4', 'C4', 'P4', 'F8', 'T4', 'T6', 'O2', 'EKG']
+
+LL = ['Fp1', 'F7', 'T3', 'T5', 'O1']
+LP = ['Fp1', 'F3', 'C3', 'P3', 'O1']
+RP = ['Fp2', 'F4', 'C4', 'P4', 'O2']
+RR = ['Fp2', 'F8', 'T4', 'T6', 'O2']
+IMPT_ELECTRODES = ['Fp1', 'O1', 'Fp2', 'O2']
+
+
+PATH_TO_FILES_TRAIN_EEG = '/Users/Patron/Documents/hms/hms_data/train_eegs/'
+PATH_TO_TRAIN_CSV = '/Users/Patron/Documents/hms/hms_data/train.csv'
