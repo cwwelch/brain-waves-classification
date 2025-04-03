9c677f1392740d48761bfad84d9af419df3658f6
Patron <Patron@Patrons-MacBook-Air.local>
2025-04-03 14:15:57 -0500

    Butter Low Filter + WT

--- filters.py
+++ filters.py
@@ -1,6 +1,9 @@
-from scipy import signal
-def butter_low(mixed_tone,fs,fc):
-    w = fc / ( fs/ 2) 
-    b, a = signal.butter(5, w, 'low')
-    output = signal.filtfilt(b, a, mixed_tone)
-    return output
+from scipy.signal import butter, lfilter
+
+def butter_lowpass_filter(data, cutoff_freq=20, sampling_rate=200, order=4):
+    nyquist = 0.5 * sampling_rate
+    normal_cutoff = cutoff_freq / nyquist
+    b, a = butter(order, normal_cutoff, btype='low', analog=False)
+    filtered_data = lfilter(b, a, data, axis=0)
+    print(type(filtered_data))
+    return filtered_data
